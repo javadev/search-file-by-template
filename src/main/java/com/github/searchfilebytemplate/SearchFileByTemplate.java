f8c47d0e861b4093a5e273e77116c8eb6546e0db
Valentyn Kolesnikov <valentyn.kolesnikov@kruschecompany.com>
2012-12-30 09:22:49 +0200

    Some code improvements

--- src/main/java/com/github/searchfilebytemplate/SearchFileByTemplate.java
+++ src/main/java/com/github/searchfilebytemplate/SearchFileByTemplate.java
@@ -111,10 +111,7 @@ public class SearchFileByTemplate extends javax.swing.JFrame {
     private static void setLookAndFeel() {
         javax.swing.UIManager.LookAndFeelInfo[] infos = UIManager.getInstalledLookAndFeels();
         String firstFoundClass = null;
-        javax.swing.UIManager.LookAndFeelInfo[] arr = infos;
-        int len = arr.length;
-        for (int index = 0; index < len; index++) {
-            javax.swing.UIManager.LookAndFeelInfo info = arr[index];
+        for (javax.swing.UIManager.LookAndFeelInfo info : infos) {
             String foundClass = info.getClassName();
             if ("com.sun.java.swing.plaf.nimbus.NimbusLookAndFeel".equals(foundClass)) {
                 firstFoundClass = foundClass;
