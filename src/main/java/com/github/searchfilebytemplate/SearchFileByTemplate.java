4a76a8a6b14f8f0a1aa06ece50821f24e0bf5d0e
Valentyn Kolesnikov <valentyn.kolesnikov@kruschecompany.com>
2012-12-30 09:27:42 +0200

    Fixed select for Nimbus Look&Feel

--- src/main/java/com/github/searchfilebytemplate/SearchFileByTemplate.java
+++ src/main/java/com/github/searchfilebytemplate/SearchFileByTemplate.java
@@ -113,7 +113,7 @@ public class SearchFileByTemplate extends javax.swing.JFrame {
         String firstFoundClass = null;
         for (javax.swing.UIManager.LookAndFeelInfo info : infos) {
             String foundClass = info.getClassName();
-            if ("com.sun.java.swing.plaf.nimbus.NimbusLookAndFeel".equals(foundClass)) {
+            if ("Nimbus".equals(info.getName())) {
                 firstFoundClass = foundClass;
                 break;
             }
